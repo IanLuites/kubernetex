d02e516ad85f1d90889b7a9d8bddbcb5fe6a2281
Ian Luites <IanLuites@users.noreply.github.com>
2019-01-14 08:37:32 +0100

    Add cluster convenience methods.

---
+++ lib/kubernetex/cluster/convenience.ex
@@ -0,0 +1,26 @@
+defmodule Kubernetex.Cluster.Convenience do
+  defmacro __using__(_opts \\ []) do
+    Enum.reduce(
+      [Kubernetex.Deployment, Kubernetex.Ingress, Kubernetex.Namespace, Kubernetex.Service],
+      nil,
+      fn resource, acc ->
+        base =
+          resource
+          |> to_string
+          |> String.split(".", trim: true)
+          |> List.last()
+          |> Macro.underscore()
+
+        quote do
+          unquote(acc)
+
+          def unquote(:"#{base}")(name, opts \\ []), do: read(unquote(resource), name, opts)
+          def unquote(:"#{base}s")(opts \\ []), do: list(unquote(resource), opts)
+
+          def unquote(:"create_#{base}")(data, opts \\ []),
+            do: create(unquote(resource), data, opts)
+        end
+      end
+    )
+  end
+end
