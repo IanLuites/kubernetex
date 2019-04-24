e3aa9d7410f91492601128c705d2dedc90a26770
Ian Luites <IanLuites@users.noreply.github.com>
2019-04-24 18:17:46 +0200

    Update deployment status.

--- lib/kubernetex/schemas/deployment/status.ex
+++ lib/kubernetex/schemas/deployment/status.ex
@@ -4,11 +4,11 @@ defmodule Kubernetex.Deployment.Status do
   defstructure version: "app/v1" do
     field :collision_count, :non_neg_integer, required: false, default: 0
     field :observed_generation, :non_neg_integer, required: true
-    field :replicas, :non_neg_integer, required: true
-    field :available_replicas, :non_neg_integer, required: true
+    field :replicas, :non_neg_integer, required: false, default: 0
+    field :available_replicas, :non_neg_integer, required: false, default: 0
     field :unavailable_replicas, :non_neg_integer, required: false, default: 0
-    field :ready_replicas, :non_neg_integer, required: true
-    field :updated_replicas, :non_neg_integer, required: true
-    field :conditions, {:array, Kubernetex.Deployment.Condition}, required: true
+    field :ready_replicas, :non_neg_integer, required: false, default: 0
+    field :updated_replicas, :non_neg_integer, required: false, default: 0
+    field :conditions, {:array, Kubernetex.Deployment.Condition}, required: false, default: []
   end
 end
